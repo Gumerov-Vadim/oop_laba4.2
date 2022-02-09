3efeaaa6da0d392c235a2a272c1f572a539b9250
Gumerov Vadim <vadim.ugatu@mail.ru>
2022-02-10 01:02:27 +0500

    add all files

---
+++ gui cs/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace gui_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Condition = new System.Windows.Forms.Label();
+            this.nud_c = new System.Windows.Forms.NumericUpDown();
+            this.nud_a = new System.Windows.Forms.NumericUpDown();
+            this.nud_b = new System.Windows.Forms.NumericUpDown();
+            this.track_c = new System.Windows.Forms.TrackBar();
+            this.track_b = new System.Windows.Forms.TrackBar();
+            this.track_a = new System.Windows.Forms.TrackBar();
+            this.tb_c = new System.Windows.Forms.TextBox();
+            this.tb_b = new System.Windows.Forms.TextBox();
+            this.tb_a = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_c)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_a)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_b)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_c)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_b)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_a)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Condition
+            // 
+            this.Condition.AutoSize = true;
+            this.Condition.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Condition.Location = new System.Drawing.Point(119, 9);
+            this.Condition.Name = "Condition";
+            this.Condition.Size = new System.Drawing.Size(581, 108);
+            this.Condition.TabIndex = 0;
+            this.Condition.Text = "A <= B <= C";
+            // 
+            // nud_c
+            // 
+            this.nud_c.Location = new System.Drawing.Point(550, 221);
+            this.nud_c.Name = "nud_c";
+            this.nud_c.Size = new System.Drawing.Size(150, 20);
+            this.nud_c.TabIndex = 1;
+            this.nud_c.ValueChanged += new System.EventHandler(this.nud_c_ValueChanged);
+            // 
+            // nud_a
+            // 
+            this.nud_a.Location = new System.Drawing.Point(137, 221);
+            this.nud_a.Name = "nud_a";
+            this.nud_a.Size = new System.Drawing.Size(136, 20);
+            this.nud_a.TabIndex = 2;
+            this.nud_a.ValueChanged += new System.EventHandler(this.nud_a_ValueChanged);
+            // 
+            // nud_b
+            // 
+            this.nud_b.Location = new System.Drawing.Point(342, 221);
+            this.nud_b.Name = "nud_b";
+            this.nud_b.Size = new System.Drawing.Size(150, 20);
+            this.nud_b.TabIndex = 3;
+            this.nud_b.ValueChanged += new System.EventHandler(this.nud_b_ValueChanged);
+            // 
+            // track_c
+            // 
+            this.track_c.Location = new System.Drawing.Point(550, 328);
+            this.track_c.Maximum = 100;
+            this.track_c.Name = "track_c";
+            this.track_c.Size = new System.Drawing.Size(150, 45);
+            this.track_c.TabIndex = 4;
+            this.track_c.Scroll += new System.EventHandler(this.track_c_Scroll);
+            // 
+            // track_b
+            // 
+            this.track_b.Location = new System.Drawing.Point(342, 328);
+            this.track_b.Maximum = 100;
+            this.track_b.Name = "track_b";
+            this.track_b.Size = new System.Drawing.Size(150, 45);
+            this.track_b.TabIndex = 5;
+            this.track_b.Scroll += new System.EventHandler(this.track_b_Scroll);
+            // 
+            // track_a
+            // 
+            this.track_a.Location = new System.Drawing.Point(137, 328);
+            this.track_a.Maximum = 100;
+            this.track_a.Name = "track_a";
+            this.track_a.Size = new System.Drawing.Size(150, 45);
+            this.track_a.TabIndex = 6;
+            this.track_a.Scroll += new System.EventHandler(this.track_a_Scroll);
+            // 
+            // tb_c
+            // 
+            this.tb_c.Location = new System.Drawing.Point(550, 264);
+            this.tb_c.Name = "tb_c";
+            this.tb_c.Size = new System.Drawing.Size(150, 20);
+            this.tb_c.TabIndex = 7;
+            this.tb_c.TextChanged += new System.EventHandler(this.tb_c_TextChanged);
+            // 
+            // tb_b
+            // 
+            this.tb_b.Location = new System.Drawing.Point(342, 264);
+            this.tb_b.Name = "tb_b";
+            this.tb_b.Size = new System.Drawing.Size(150, 20);
+            this.tb_b.TabIndex = 8;
+            this.tb_b.TextChanged += new System.EventHandler(this.tb_b_TextChanged);
+            // 
+            // tb_a
+            // 
+            this.tb_a.Location = new System.Drawing.Point(137, 264);
+            this.tb_a.Name = "tb_a";
+            this.tb_a.Size = new System.Drawing.Size(136, 20);
+            this.tb_a.TabIndex = 9;
+            this.tb_a.TextChanged += new System.EventHandler(this.tb_a_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_a);
+            this.Controls.Add(this.tb_b);
+            this.Controls.Add(this.tb_c);
+            this.Controls.Add(this.track_a);
+            this.Controls.Add(this.track_b);
+            this.Controls.Add(this.track_c);
+            this.Controls.Add(this.nud_b);
+            this.Controls.Add(this.nud_a);
+            this.Controls.Add(this.nud_c);
+            this.Controls.Add(this.Condition);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_c)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_a)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_b)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_c)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_b)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_a)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Condition;
+        private System.Windows.Forms.NumericUpDown nud_c;
+        private System.Windows.Forms.NumericUpDown nud_a;
+        private System.Windows.Forms.NumericUpDown nud_b;
+        private System.Windows.Forms.TrackBar track_c;
+        private System.Windows.Forms.TrackBar track_b;
+        private System.Windows.Forms.TrackBar track_a;
+        private System.Windows.Forms.TextBox tb_c;
+        private System.Windows.Forms.TextBox tb_b;
+        private System.Windows.Forms.TextBox tb_a;
+    }
+}
+
---
+++ gui cs/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using gui_cs.Properties;
+
+namespace gui_cs
+{
+    public partial class Form1 : Form
+    {
+        Model model;
+        public Form1()
+        {
+            InitializeComponent();
+            model = new Model();
+            model.observers += new System.EventHandler(this.update_from_model);
+        }
+
+        private void nud_a_ValueChanged(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(nud_a.Value);
+            model.set(a, 1);
+        }
+
+        private void nud_b_ValueChanged(object sender, EventArgs e)
+        {
+            int b = Convert.ToInt32(nud_b.Value);
+            model.set(b, 2);
+
+        }
+
+        private void nud_c_ValueChanged(object sender, EventArgs e)
+        {
+            int c = Convert.ToInt32(nud_c.Value);
+            model.set(c, 3);
+        }
+        private void tb_a_TextChanged(object sender, EventArgs e)
+        {
+            int a;
+            bool isNum = int.TryParse(tb_a.Text, out a);
+
+            if (isNum)
+            {
+                a = Convert.ToInt32(tb_a.Text);
+                model.set(a, 1);
+            }
+
+        }
+        private void tb_b_TextChanged(object sender, EventArgs e)
+        {
+            int b;
+            bool isNum = int.TryParse(tb_b.Text, out b);
+
+            if (isNum)
+            {
+                b = Convert.ToInt32(tb_b.Text);
+                model.set(b, 2);
+            }
+
+        }
+        private void tb_c_TextChanged(object sender, EventArgs e)
+        {
+            int c;
+            bool isNum = int.TryParse(tb_c.Text, out c);
+
+            if (isNum)
+            {
+                c = Convert.ToInt32(tb_c.Text);
+                model.set(c, 3);
+            }
+
+        }
+        private void track_a_Scroll(object sender, EventArgs e)
+        {
+            model.set(track_a.Value, 1);
+        }
+        private void track_b_Scroll(object sender, EventArgs e)
+        {
+            model.set(track_b.Value, 2);
+        }
+        private void track_c_Scroll(object sender, EventArgs e)
+        {
+            model.set(track_c.Value, 3);
+        }
+        private void update_from_model(object sender, EventArgs e)
+        {
+            nud_a.Value = model.get(1);
+            nud_b.Value = model.get(2);
+            nud_c.Value = model.get(3);
+            tb_a.Text = model.get(1).ToString();
+            tb_b.Text = model.get(2).ToString();
+            tb_c.Text = model.get(3).ToString();
+            track_a.Value = model.get(1);
+            track_b.Value = model.get(2);
+            track_c.Value = model.get(3);
+
+        }
+        public class Model
+        {
+            private
+                int a, b, c;
+                void set()
+            {
+                if (c > 100) { c = 100; }
+                if (b > c)
+                {
+                    b = c;
+                }
+                if (a > b)
+                {
+                    a = b;
+                }
+                Settings.Default["num_a"] = this.a;
+                Settings.Default["num_b"] = this.b;
+                Settings.Default["num_c"] = this.c;
+                Settings.Default.Save();
+            }
+            public System.EventHandler observers;
+            public void set(int el, int a)
+            {
+                if (el >= 0)
+                {
+                    switch (a)
+                    {
+                        case 1:
+                            if (el > b)
+                            {
+                                this.a = b;
+                            }
+                            else
+                            {
+                                this.a = el;
+                            }
+                            set();
+                            break;
+                        case 2:
+                            if (el > c)
+                            {
+                                b = c;
+                            }
+                            else
+                            {
+                                this.b = el;
+                            }
+                            set();
+                            break;
+                        case 3:
+                            this.c = el;
+                            set();
+                            break;
+                        default:
+                            break;
+                    }
+                    observers.Invoke(this, null);
+                }
+            }
+            public int get(int i)
+            {
+                switch (i)
+                {
+                    case 1:
+                        return this.a;
+                        break;
+                    case 2:
+                        return this.b;
+                        break;
+                    case 3:
+                        return this.c;
+                        break;
+                    default:
+                        return 0;
+                        break;
+                }
+            }
+            public void check_update()
+            {
+                observers.Invoke(this, null);
+            }
+            public Model()
+            {
+                int num_a = Convert.ToInt32(Settings.Default["num_a"]);
+                int num_b = Convert.ToInt32(Settings.Default["num_b"]);
+                int num_c = Convert.ToInt32(Settings.Default["num_c"]);
+                this.a = num_a;
+                this.b = num_b;
+                this.c = num_c;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            model.check_update();
+        }
+    }
+}
